0d0d1bb2954070f6397757048d9a84857770b173
J-R77-stack <johnnyramsay81@gmail.com>
2022-10-15 12:02:28 +0000

    adding and deleting new tasks

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -3,15 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-list';
   tasks = [
-    "Visit Ann",
-    "Call Dad",
-    "Go to the gym",
-    "Wash the dishes",
-    "Shop for the party"
-  ]
+    'Visit Ann',
+    'Call Dad',
+    'Go to the gym',
+    'Wash the dishes',
+    'Shop for the party',
+  ];
+
+  add(newTask: string) {
+    this.tasks.push(newTask);
+  }
+
+  remove(existingTask: string) {
+    var userConfirmed = confirm(
+      `Are you sure you want to remove the following task? \n "${existingTask}"`
+    );
+
+    if (userConfirmed) {
+      this.tasks = this.tasks.filter((task) => task != existingTask);
+    }
+  }
 }
